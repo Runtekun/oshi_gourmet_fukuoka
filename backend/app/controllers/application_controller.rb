314bf8ea7f3a1d22c612d53bdae189739cf80c62
dec457108 <yuuki1997s@i.softbank.jp>
2026-01-07 19:53:52 +0900

    Cookieベースのログイン（Devise）設定

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+  # APIモードでもDeviseのセッション認証（Cookie）を使うために追加
+  include ActionController::Cookies
+  # Cookieを使った認証を安全に行うためのCSRF対策のため追加
+  include ActionController::RequestForgeryProtection
 end
