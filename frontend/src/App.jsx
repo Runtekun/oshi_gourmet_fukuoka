d87d9f57d8f28b4f7f7a39eafb2f72a77f36427d
工藤 <kudo@kudounoMacBook-Air.local>
2026-01-23 00:23:14 +0900

    ルーティングを追加

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from "./pages/LandingPage";
 import { AuthProvider } from "./contexts/AuthContext";
 import SignUp from "./components/auth/SignUp";
+import SignIn from "./components/auth/SignIn";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
