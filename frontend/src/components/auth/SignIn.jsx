3adcd1179fe59af1fff57a2bf91072d96c67a724
工藤 <kudo@kudounoMacBook-Air.local>
2026-01-26 00:27:06 +0900

    ログイン成功後に/homeへ遷移する処理を追加

--- frontend/src/components/auth/SignIn.jsx
+++ frontend/src/components/auth/SignIn.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { useAuth } from "../../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function SingIn() {
   const { login, isLoading } = useAuth();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -19,6 +21,7 @@ export default function SingIn() {
     try {
       await login({ email, password });
       alert("ログイン成功！");
+      navigate("/home");
     } catch {
       setError("ログインに失敗しました");
     }
