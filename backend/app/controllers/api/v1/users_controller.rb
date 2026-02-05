c26d3c6e10c3b914c24c7b98778bd4b25d2f9a2d
工藤 <kudo@kudounoMacBook-Air.local>
2026-02-05 20:22:09 +0900

    users_controllerを実装

---
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::UsersController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_user
+  
+    def show
+      render json: @user
+    end
+  
+    def update
+      if @user.update(user_params)
+        render json: @user
+      else
+        render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+    private
+  
+    def set_user
+      @user = current_user
+    end
+  
+    def user_params
+      params.require(:user).permit(:name, :avatar)
+    end
+end
