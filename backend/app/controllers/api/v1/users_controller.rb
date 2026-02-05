bf574abc594cfe4ebaf0a2b73951361981dce74e
工藤 <kudo@kudounoMacBook-Air.local>
2026-02-05 22:56:05 +0900

    userテーブルにカラムを追加

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -21,6 +21,6 @@ class Api::V1::UsersController < ApplicationController
     end
   
     def user_params
-      params.require(:user).permit(:name, :avatar)
+      params.require(:user).permit(:name, :avatar, :introduction) 
     end
 end
